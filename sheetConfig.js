5af51413daaaa8988848d0ad8caee999adea0178
Bassistedric <cedric.comble@gmail.com>
2025-09-05 12:06:01 +0200

    feat: add sheet configuration file

---
+++ sheetConfig.js
@@ -0,0 +1,8 @@
+export const SHEET_BASE_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTNJqvjeu_Hox7I64HhX3y4j1SBWHswa6QO1mPwHEt2siXMP609WT__DnuGK-0Brlfq5D1a2R_iyL3g/pub?output=csv";
+
+export const METIER_TO_GID = {
+  Elec: "1111111111",
+  HVAC_REF: "2222222222"
+};
+
+export const TBM_GID = "3333333333";
