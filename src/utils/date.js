bfcff73e653fbd0d8ab54a4349b3a1d89678d833
Bassistedric <cedric.comble@gmail.com>
2025-09-08 20:25:56 +0200

    Factorize date helper

---
+++ src/utils/date.js
@@ -0,0 +1,6 @@
+export const nowLocalDateTime = () => {
+  const d = new Date(),
+    pad = (n) => String(n).padStart(2, "0");
+  return `${d.getFullYear()}-${pad(d.getMonth() + 1)}-${pad(d.getDate())}T${pad(d.getHours())}:${pad(d.getMinutes())}`;
+};
+
