74f409b3e51af2908d6631916facc5580e2d5a7a
Bassistedric <cedric.comble@gmail.com>
2026-06-22 19:04:49 +0200

    Add PM selection to FMRA

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/main.jsx',
+  entry: './src/utils/main.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
